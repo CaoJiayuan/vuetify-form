72768cb6024085aae9b23cfa733b497b7b943a46
cjy <cjy632258@hotmail.com>
2018-08-22 16:13:02 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import VuetifyForm from './lib/form';
+import {register} from './lib/validator'
 
 VuetifyForm.install = function (Vue) {
   Vue.component('vuetify-form', VuetifyForm)
+}
+VuetifyForm.validator = {
+  register
 }
 
 export default VuetifyForm
